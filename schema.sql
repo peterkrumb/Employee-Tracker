28890337d104f1edb2dec34a78e4dda6caaa422f
peterkrumb <petekrumb@gmail.com>
2020-01-25 23:01:54 -0600

    added more functions to CLI

--- schema.sql
+++ schema.sql
@@ -3,23 +3,23 @@ USE Employee_TrackerDB;
 
 CREATE TABLE department
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT NULL,
 	name varchar(30) NOT NULL,
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE role
+CREATE TABLE roles
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT NULL,
 	title varchar(30) NOT NULL,
-    salary decimal(5, 2),
+    salary decimal(10, 3),
     department_id INT NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE employee
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT NULL,
 	first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL,
     role_id INT NOT NULL,
--- server.js
+++ server.js
@@ -30,8 +30,9 @@ function runSearch() {
             choices: [
                 "View All Employees",
                 "View All Employees By Department",
-                //"View All Employees By Manager",
-                //"Add Employee",
+                "View All Employees By Manager",
+                "View Departments",
+                "View Roles",
                 //"Update Employee Role",
                 //"Update Employee Manager"
             ]
@@ -42,17 +43,17 @@ function runSearch() {
                     viewEmployees();
                     break;
 
-                    // case "Find all artists who appear more than once":
-                    //     multiSearch();
-                    //     break;
+                case "View All Employees By Department":
+                    viewEmpByDept();
+                    break;
 
-                    // case "Find data within a specific range":
-                    //     rangeSearch();
-                    //     break;
+                case "View Departments":
+                    viewDepts();
+                    break;
 
-                    // case "Search for a specific song":
-                    //     songSearch();
-                    //     break;
+                case "View Roles":
+                    viewRoles();
+                    break;
 
                     // case "Find artists with a top song and top album in the same year":
                     //     songAndAlbumSearch();
@@ -62,5 +63,87 @@ function runSearch() {
 }
 
 function viewEmployees() {
+    //<--Manager isn't a table, so we set it to a temporary alias manager which gets set to a manager id on line 73-->
+    var query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager `
+        //<--Connects employee's role id to corresponding id-->
+    query += "FROM employee LEFT JOIN role ON employee.role_id = role.id ";
+    //<--Connects the role to its corresponding department-->
+    query += "LEFT JOIN department ON department.id = role.department_id ";
+    //<-- Connects the employee's manager id to the corresponding manager-->
+    query += "LEFT JOIN employee AS manager ON manager.id = employee.manager_id";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res)
+        runSearch();
+    })
+}
+
+function viewEmpByDept() {
+    inquirer.prompt({
+            name: "departments",
+            type: "list",
+            message: "Please select the department of the employee",
+            choices: ["Sales",
+                "Engineering",
+                "Finance",
+                "Legal"
+            ]
+        })
+        .then(function(answer) {
+            const department = answer.departments;
+            var query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary `
+            query += `FROM employee LEFT JOIN role ON employee.role_id = role.id `;
+            query += `LEFT JOIN department ON role.department_id = department.id WHERE department.name="${department}"`;
+            connection.query(query, function(err, res) {
+                if (err) throw err;
+                console.table(res);
+                runSearch();
+            })
+        })
 
 }
+
+function viewDepts() {
+    connection.query(`SELECT department.id, department.name FROM department`, function(err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        runSearch();
+    });
+}
+
+function viewRoles() {
+    connection.query(`SELECT role.id, role.title FROM role`, function(err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        runSearch();
+    });
+}
+
+// function ViewEmpByMan() {
+//     inquirer.prompt({
+//             name: "manager",
+//             type: "list",
+//             message: "Please select the manager of the employee",
+//             choices: ["Ashley Rodriguez",
+//                 "John Doe",
+//                 "Sarah Lourd",
+//                 "Mike Chan"
+//             ]
+//         })
+//         .then(function(answer) {
+//             const manager = answer.manager;
+//             var query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary `
+//             query += `FROM employee LEFT JOIN role ON employee.role_id = role.id `;
+//             query += `LEFT JOIN employee AS manager ON manager.id = employee.manager_id"${manager}"`;
+//             connection.query(query, function(err, res) {
+//                 if (err) {
+//                     throw err
+//                 }
+//                 // LOGS RESULTS
+//                 console.table(res);
+//             })
+//         })
+
+//     }
